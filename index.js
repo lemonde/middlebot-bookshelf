a728072a8df609770f736ff00864f6deeca73ae4
Greg Bergé <neoziro@gmail.com>
2014-05-09 10:53:06 +0200

    Use "exports" instead of "module.exports".

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
-'use strict';
-
 /**
- * CRUD middlewares to access bookshelf models
+ * Expose module.
  */
 
-module.exports = {
-  create: require('./lib/create'),
-  destroy: require('./lib/destroy'),
-  search: require('./lib/search'),
-  find: require('./lib/find'),
-  findAll: require('./lib/find-all'),
-  snakeCaseKeys: require('./lib/snake-case-keys'),
-  camelizeKeys: require('./lib/camelize-keys'),
-  formatFindOptions: require('./lib/format-find-options'),
-  formatFindAllOptions: require('./lib/format-find-all-options'),
-  formatBackboneModel: require('./lib/format-backbone-model'),
-  formatBackboneCollection: require('./lib/format-backbone-collection')
-};
-
+exports.create = require('./lib/create');
+exports.destroy = require('./lib/destroy');
+exports.search = require('./lib/search');
+exports.find = require('./lib/find');
+exports.findAll = require('./lib/find-all');
+exports.snakeCaseKeys = require('./lib/snake-case-keys');
+exports.camelizeKeys = require('./lib/camelize-keys');
+exports.formatFindOptions = require('./lib/format-find-options');
+exports.formatFindAllOptions = require('./lib/format-find-all-options');
+exports.formatBackboneModel = require('./lib/format-backbone-model');
+exports.formatBackboneCollection = require('./lib/format-backbone-collection');
