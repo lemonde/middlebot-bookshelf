edb9227d276d79864b8b1b8a0c4339baf65123c0
Yannick Dominguez <yannick.dominguez@gmail.com>
2014-05-12 11:45:22 +0200

    updated existing tests and split them in separate files

---
+++ test/camelize-keys.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var chai = require('chai');
+var expect = chai.expect;
+
+var camelizeKeys = require('../lib/camelize-keys');
+
+describe('#camelizeKeys', function () {
+  it('should camelize all keys', function () {
+    var input = {
+      first_name: 'Johny',
+      last_name: 'Begood'
+    };
+
+    input = camelizeKeys(input);
+    expect(input).to.eql({
+      firstName: 'Johny',
+      lastName: 'Begood'
+    });
+  });
+
+  it('should work with deep', function () {
+    var input = {
+      ar: [
+        {
+          big_test: true
+        }
+      ],
+      foo: { bar_true: true }
+    };
+    input = camelizeKeys(input);
+    expect(input).to.eql({
+      ar: [
+        {
+          bigTest: true
+        }
+      ],
+      foo: { barTrue: true }
+    });
+  });
+});
