2fbe7578657b5125bf951e76de34c9948cdb2526
Greg Bergé <neoziro@gmail.com>
2014-05-12 18:47:18 +0200

    Put formatters in a separated folder.

--- test/formatters/backbone.js
+++ test/formatters/backbone.js
@@ -14,14 +14,23 @@ var bookshelf = Bookshelf.initialize({
   }
 });
 
-var formatBackboneCollection = require('../lib/format-backbone-collection');
+var backboneFormatter = require('../../lib/formatters/backbone');
+
+describe('#formatBackboneModel', function () {
+  it('should convert model to JSON', function () {
+    var model = new bookshelf.Model({ big_foo: 'bar' });
+
+    model = backboneFormatter.formatModel(model);
+    expect(model).to.eql({ bigFoo: 'bar' });
+  });
+});
 
 describe('#formatBackboneCollection', function () {
   it('should convert collection to JSON', function () {
     var model = new bookshelf.Model({ big_foo: 'bar' });
     var collection = new bookshelf.Collection([model]);
 
-    collection = formatBackboneCollection(collection);
+    collection = backboneFormatter.formatCollection(collection);
     expect(collection).to.eql([{ bigFoo: 'bar' }]);
 
   });
