bc67bde080f839fd311ab2b18c188492541cdb88
Greg Bergé <neoziro@gmail.com>
2014-05-13 10:14:26 +0200

    Externalize where formatter.

---
+++ test/formatters/where.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var expect = require('chai').expect;
+var whereFormatter = require('../../lib/formatters/where');
+
+describe('Where formatter', function () {
+  it('should format where', function () {
+    expect(whereFormatter(['opt'], { bigTest: 'foo', opt: false })).to.eql({ big_test: 'foo' });
+  });
+});
