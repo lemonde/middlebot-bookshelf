ffe70bf9d1384fd6e781005b4456da0fe660aa45
Greg Bergé <neoziro@gmail.com>
2014-05-13 09:58:50 +0200

    Refactor update, support withRelated.

---
+++ test/formatters/bookshelf-options.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var expect = require('chai').expect;
+var bkOptsFormatter = require('../../lib/formatters/bookshelf-options');
+
+describe('Bookshelf options formatter', function () {
+  it('should format options', function () {
+    expect(bkOptsFormatter(['foo'], { foo: 'bar' }, { foo: 'test' })).to.eql({ foo: 'bar' });
+  });
+
+  it('should format withRelated correctly', function () {
+    expect(bkOptsFormatter(['withRelated'], { withRelated: ['bigTest'] }, {}))
+    .to.eql({ withRelated: ['big_test'] });
+  });
+});
---
+++ test/middlewares/update.js
@@ -0,0 +1,52 @@
+var expect = require('chai').expect;
+var middlebot = require('middlebot');
+var updateMiddleware = require('../../lib/middlewares/update');
+var db = require('../fixtures/database');
+
+describe('update middleware', function () {
+  var app, req, res;
+
+  beforeEach(db.reset);
+
+  beforeEach(function () {
+    app = middlebot();
+    req = {};
+    res = {};
+  });
+
+  it('should update an author', function (done) {
+    req.body = { longName: 'Classe man, top of the pop' };
+    req.query = { id: 1 };
+
+    app.use(updateMiddleware({ model: db.Author }));
+
+    app.handle('default', req, res, function (err) {
+      if (err) return done(err);
+      expect(res.body).to.have.property('longName', 'Classe man, top of the pop');
+      done();
+    });
+  });
+
+  it('should return an error if the author to update is not found', function (done) {
+    req.query = { id: 10 };
+
+    app.use(updateMiddleware({ model: db.Author, error: new Error('no author') }));
+
+    app.handle('default', req, res, function (err) {
+      expect(err).to.have.property('message', 'no author');
+      done();
+    });
+  });
+
+  it('should support withRelated', function (done) {
+    req.query = { id: 1, withRelated: 'user' };
+
+    app.use(updateMiddleware({ model: db.Author }));
+
+    app.handle('default', req, res, function (err) {
+      if (err) return done(err);
+      expect(res.body.user).to.exists;
+      done();
+    });
+  });
+});
