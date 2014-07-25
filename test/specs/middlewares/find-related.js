f26f74e7893efbf7df2bdcc31d8734b3322f62d1
Yannick Dominguez <yannick.dominguez@gmail.com>
2014-07-25 13:48:18 +0200

    finished implementing find-related middleware

--- test/specs/middlewares/find-related.js
+++ test/specs/middlewares/find-related.js
@@ -3,7 +3,7 @@ var findRelated = require('../../../index').findRelated;
 var db = require('../../fixtures/database');
 var createServer = require('../../utils/http').createServer;
 
-describe.only('find with pivot middleware', function () {
+describe('find with related middleware', function () {
   beforeEach(db.reset);
 
   it('should be possible to find a related object with pivot table data', function (done) {
@@ -71,9 +71,164 @@ describe.only('find with pivot middleware', function () {
     }, done);
   });
 
-  it('should be possible to count the number of related objects');
-  it('should be possible to sort the related objects');
-  it('should be possible to offset the related objects');
-  it('should be possible to offset the related objects');
-  it('should be possible to do a where');
+  it('should be possible to count the number of related objects', function (done) {
+    var server = createServer(findRelated({ model: db.Article, related: 'authors' }), {
+      params: { id: 1 },
+      query: {count: true}
+    });
+
+    request(server)
+    .get('/')
+    .expect(200, {
+      body: [{
+        PivotArticleId: 1,
+        PivotAuthorId: 2,
+        createdAt: null,
+        id: 2,
+        longName: 'George Abitbol2',
+        origin: 'L\'homme le plus classe du monde2',
+        shortName: 'G.A.2',
+        updatedAt: null,
+        userId: 1
+      }, {
+        PivotArticleId: 1,
+        PivotAuthorId: 1,
+        createdAt: null,
+        id: 1,
+        longName: 'George Abitbol',
+        origin: 'L\'homme le plus classe du monde',
+        shortName: 'G.A.',
+        updatedAt: null,
+        userId: null
+      }],
+      metadata: {
+        count: 2,
+        limit: 20,
+        offset: 0
+      }
+    }, done);
+
+  });
+
+  it('should be possible to sort the related objects', function (done) {
+    var server = createServer(findRelated({ model: db.Article, related: 'authors' }), {
+      params: { id: 1 },
+      query: {sortBy:'longName'}
+    });
+
+    request(server)
+    .get('/')
+    .expect(200, {
+      body: [{
+        PivotArticleId: 1,
+        PivotAuthorId: 2,
+        createdAt: null,
+        id: 2,
+        longName: 'George Abitbol2',
+        origin: 'L\'homme le plus classe du monde2',
+        shortName: 'G.A.2',
+        updatedAt: null,
+        userId: 1
+      }, {
+        PivotArticleId: 1,
+        PivotAuthorId: 1,
+        createdAt: null,
+        id: 1,
+        longName: 'George Abitbol',
+        origin: 'L\'homme le plus classe du monde',
+        shortName: 'G.A.',
+        updatedAt: null,
+        userId: null
+      }],
+      metadata: {
+        count: null,
+        limit: 20,
+        offset: 0
+      }
+    }, done);
+  });
+  it('should be possible to offset the related objects', function (done) {
+    var server = createServer(findRelated({ model: db.Article, related: 'authors' }), {
+      params: { id: 1 },
+      query: {limit: 1, offset:1}
+    });
+
+    request(server)
+    .get('/')
+    .expect(200, {
+      body: [{
+        PivotArticleId: 1,
+        PivotAuthorId: 1,
+        createdAt: null,
+        id: 1,
+        longName: 'George Abitbol',
+        origin: 'L\'homme le plus classe du monde',
+        shortName: 'G.A.',
+        updatedAt: null,
+        userId: null
+      }],
+      metadata: {
+        count: null,
+        limit: 1,
+        offset: 1
+      }
+    }, done);
+  });
+
+  it('should be possible to do a where', function (done) {
+
+    var server = createServer(findRelated({ model: db.Article, related: 'authors' }), {
+      params: { id: 1 },
+      query: {shortName: 'G.A.'}
+    });
+
+    request(server)
+    .get('/')
+    .expect(200, {
+      body: [{
+        PivotArticleId: 1,
+        PivotAuthorId: 1,
+        createdAt: null,
+        id: 1,
+        longName: 'George Abitbol',
+        origin: 'L\'homme le plus classe du monde',
+        shortName: 'G.A.',
+        updatedAt: null,
+        userId: null
+      }],
+      metadata: {
+        count: null,
+        limit: 20,
+        offset: 0
+      }
+    }, done);
+  });
+
+  it('should be possible to only get one of the related items', function (done) {
+    var server = createServer(findRelated({ model: db.Article, related: 'authors' }), {
+      params: { id: 1, relatedIndex: 2 }
+    });
+
+    request(server)
+    .get('/')
+    .expect(200, {
+      body: {
+        PivotArticleId: 1,
+        PivotAuthorId: 1,
+        createdAt: null,
+        id: 1,
+        longName: 'George Abitbol',
+        origin: 'L\'homme le plus classe du monde',
+        shortName: 'G.A.',
+        updatedAt: null,
+        userId: null
+      },
+      metadata: {
+        count: null,
+        limit: 20,
+        offset: 0
+      }
+    }, done);
+
+  });
 });
